0e479c1fd28b4f0ee98e163d957f11256162ad14
rosario <rosarioayyala@gmail.com>
2021-05-22 14:41:14 -0300

    BQA-15:inventory-item-1/2

---
+++ test/pageobjects/inventory-item.page.js
@@ -0,0 +1,36 @@
+const Page = require('./page');
+const LoginPage = require('../pageobjects/login.page');
+
+
+class InventoryPageItem extends Page {
+  
+    get BurgerBtn () { return $('#react-burger-menu-btn') } 
+    get AllitemsLink () { return $('#inventory_sidebar_link') }
+    get AboutLink () { return $('#about_sidebar_link') }
+    get LogoutLink () { return $('#logout_sidebar_link') }
+    get ResetLink () { return $('#reset_sidebar_link') }
+    get AddbackpacktBtn () { return $('#add-to-cart-sauce-labs-backpack')}
+    get CartLink () { return $('.shopping_cart_link') }
+    get TwitLink () { return $('.social_twitter a')}
+    get FaceLink () { return $('.social_facebook a')}
+    get LinkedLink () { return $('.social_linkedin a')}
+    get CartBadgeBtn () { return $('.shopping_cart_badge') }
+    get RemoveBtn () { return $('#remove-sauce-labs-backpack')}
+    get BackBtn (){ return $('#back-to-products')}
+    get TitleBackpackLink(){ return $("=Sauce Labs Backpack")}
+
+    
+
+    
+    open () {
+        super.open('');
+        LoginPage.inputUsername.setValue('standard_user');
+        LoginPage.inputPassword.setValue('secret_sauce');
+        LoginPage.submit();
+
+    }
+
+    
+}
+
+module.exports = new InventoryPageItem();
